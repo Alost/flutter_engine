3cb16288fa2a98bd0004019dbb45b97f404b5e0d
OoO <OoO@ooo.com>
2024-09-22 03:33:43 +0800

    test

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -19,12 +19,14 @@ lsb_release -a
 cat /etc/os-release
 lshw -short
 ifconfig
-swapon --show
 
 sudo apt-get install -y ninja-build pkg-config openjdk-21-jdk
 
-sudo fallocate -l 6G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
+sudo swapon --show
+sudo swapoff -a
+sudo fallocate -l 6G ./swapfile
+sudo chmod 600 ./swapfile
+sudo mkswap ./swapfile
+sudo swapon ./swapfile
 sudo sysctl vm.swappiness=25
+sudo swapon --show
