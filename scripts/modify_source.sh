160c97736981ec53e9d7d4456c30b925ccf2a081
OoO <OoO@ooo.com>
2024-09-21 21:00:45 +0800

    test

--- scripts/modify_source.sh
+++ scripts/modify_source.sh
@@ -11,6 +11,31 @@ export ROOT_DIR="${ROOT_DIR:-$PWD}"
 
 source $ROOT_DIR/scripts/common.sh
 
+replace_lines_with_file() {
+    local target_file="$1"
+    local start_line="$2"
+    local end_line="$3"
+    local replace_file="$4"
+    
+    sed -i "${start_line},${end_line}{ r ${replace_file}
+      d
+    }" "${target_file}"
+}
+
+replace_lines_with_string() {
+    local target_file="$1"
+    local start_line="$2"
+    local end_line="$3"
+    local replacement="$4"
+    
+    local temp_file=$(mktemp)
+    echo -e "$replacement" > "$temp_file"
+
+    replace_lines_with_file "$target_file" "$start_line" "$end_line" "$temp_file"
+
+    rm "$temp_file"
+}
+
 if [ "$1" == "git_clone" ]; then
     cd $ROOT_DIR/flutter/source/engine
     git checkout 3.22.3
