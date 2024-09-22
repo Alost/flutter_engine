deef26e45142086d9a8757aa7a9282cde04002c9
OoO <OoO@ooo.com>
2024-09-22 18:13:05 +0800

    no gn opts

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -8,7 +8,7 @@ echo "ROOT_DIR=$ROOT_DIR" >> $GITHUB_ENV
 
 export PATH=$ROOT_DIR/flutter/depot_tools:$PATH
 
-export GN_OPTS="--no-goma --no-build-embedder-examples --no-build-glfw-shell --disable-desktop-embeddings --no-enable-unittests --no-clang-static-analyzer"
+# export GN_OPTS="--no-goma --no-build-embedder-examples --no-build-glfw-shell --disable-desktop-embeddings --no-enable-unittests --no-clang-static-analyzer"
 
 export VERSION=3.22.3
 echo "VERSION=$VERSION" >> $GITHUB_ENV
