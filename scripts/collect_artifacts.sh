15befe24dcf9f59646fdd86e10a352117ed6ba28
OoO <OoO@ooo.com>
2024-09-22 17:48:08 +0800

    test

--- scripts/collect_artifacts.sh
+++ scripts/collect_artifacts.sh
@@ -2,6 +2,10 @@
 
 source ${ROOT_DIR:-$PWD}/scripts/set_env.sh
 
+log "collect artifacts start"
+
 cd $ROOT_DIR/flutter/engine/src
 rm -rf lib.tar.gz
 tar -czvf lib.tar.gz lib
+
+log "collect artifacts finish"
--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -2,6 +2,8 @@
 
 source ${ROOT_DIR:-$PWD}/scripts/set_env.sh
 
+log "prepare environment start"
+
 # CPU性能从高到低排序：7763,8370C,8272CL,8171M,E5系列
 # 好像macos性能比linux高?
 cat /proc/cpuinfo | grep 'model name'
@@ -32,3 +34,5 @@ sudo sysctl vm.swappiness=25
 sudo swapon --show
 
 sudo apt-get install -y ninja-build pkg-config openjdk-21-jdk
+
+log "prepare environment finish"
