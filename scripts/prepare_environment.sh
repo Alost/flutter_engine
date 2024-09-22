3ce245195a1804e38a2a5e20fe1af4ca4f6f5a78
OoO <OoO@ooo.com>
2024-09-22 17:05:57 +0800

    test

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -3,6 +3,7 @@
 source ${ROOT_DIR:-$PWD}/scripts/set_env.sh
 
 # CPU性能从高到低排序：7763,8370C,8272CL,8171M,E5系列
+# 好像macos性能比linux高?
 cat /proc/cpuinfo | grep 'model name'
 lscpu | grep 'Model name'
 
@@ -20,8 +21,7 @@ cat /etc/os-release
 lshw -short
 ifconfig
 
-sudo apt-get install -y ninja-build pkg-config openjdk-21-jdk
-
+# 启用交换文件，防止内存不足
 sudo swapon --show
 sudo swapoff -a
 sudo fallocate -l 6G ./swapfile
@@ -30,3 +30,5 @@ sudo mkswap ./swapfile
 sudo swapon ./swapfile
 sudo sysctl vm.swappiness=25
 sudo swapon --show
+
+sudo apt-get install -y ninja-build pkg-config openjdk-21-jdk
