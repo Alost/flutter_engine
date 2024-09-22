ba5a0e641f4f23654c064f689541ae3b24e442cc
OoO <OoO@ooo.com>
2024-09-22 20:58:59 +0800

    new with sync

--- scripts/modify_source.sh
+++ scripts/modify_source.sh
@@ -2,18 +2,6 @@
 
 source ${ROOT_DIR:-$PWD}/scripts/set_env.sh
 
-# 会进来两次，要根据对应代码路径是否存在来修改，默认是第二次调用
-# 第一次是 git clone 后
-# 第二次是 gclient sync 后
-
-if [ "$1" == "git_clone" ]; then
-    cd $ROOT_DIR/flutter/source/engine
-    git checkout $VERSION
-    
-    log "checkout source finish"
-    exit 0
-fi
-
 cd $ROOT_DIR/flutter/engine
 
 python3 $ROOT_DIR/scripts/modify_source.py
--- scripts/prepare_source.sh
+++ scripts/prepare_source.sh
@@ -8,30 +8,16 @@ log "prepare source start"
 mkdir -p $ROOT_DIR/flutter; cd $ROOT_DIR/flutter;
 git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git || true
 
-# flutter engine 代码
-mkdir -p $ROOT_DIR/flutter/source; cd $ROOT_DIR/flutter/source
-git clone https://github.com/flutter/engine.git || true
-
-# 切换版本
-($ROOT_DIR/scripts/modify_source.sh "git_clone")
-git config --global user.email "flutter@example.com" && git config --global user.name "flutter"
-git add . && git commit -am "modify"
-
 # 同步依赖
 mkdir -p $ROOT_DIR/flutter/engine; cd $ROOT_DIR/flutter/engine;
-cat <<EOL > .gclient
-solutions = [
-  {
-    "custom_deps": {},
-    "deps_file": "DEPS",
-    "managed": False,
-    "name": "src/flutter",
-    "safesync_url": "",
-    "url": "$ROOT_DIR/flutter/source/engine",
-  },
-]
-EOL
-# gclient sync
+fetch flutter
+
+gclient sync
+
+# 切换版本
+cd $ROOT_DIR/flutter/engine/src/flutter
+git checkout $VERSION
+
 gclient sync -D --with_branch_heads --with_tags
 
 # 修改代码
