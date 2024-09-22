383bbb18cee6cf1d06b890935da5ae1e9206a4f9
OoO <OoO@ooo.com>
2024-09-22 21:03:54 +0800

    new no sync

--- scripts/prepare_source.sh
+++ scripts/prepare_source.sh
@@ -8,16 +8,17 @@ log "prepare source start"
 mkdir -p $ROOT_DIR/flutter; cd $ROOT_DIR/flutter;
 git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git || true
 
-# 同步依赖
+# 下载代码和依赖
 mkdir -p $ROOT_DIR/flutter/engine; cd $ROOT_DIR/flutter/engine;
 fetch flutter
 
-gclient sync
+# gclient sync
 
 # 切换版本
 cd $ROOT_DIR/flutter/engine/src/flutter
 git checkout $VERSION
 
+# 下载依赖
 gclient sync -D --with_branch_heads --with_tags
 
 # 修改代码
